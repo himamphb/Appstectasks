813a1ad8435a95e50a88c7bd6f49612b382b14eb
himamphb <himamphb124@gmail.com>
2023-05-05 17:56:05 +0530

    code

--- Projects/DOM/inputSearch/search.js
+++ Projects/DOM/inputSearch/search.js
@@ -1,27 +1,54 @@
-
 let arr = [];
+let orederedList = document.getElementById("citylist");
 
-function submitCity(){
-    let cityarray = document.getElementById("cityname").value;
-    let orederedlist = document.getElementById("citylist");
-    arr.push(cityarray);
-    
-    let lielement = document.createElement("li");
-    lielement.textContent = cityarray;
-    orederedlist.appendChild(lielement);
-    document.getElementById("cityname").value = "";
-
+function clearInputValue() {
+  document.getElementById("citysearch").value = "";
 }
-function submitSearch(){
-    let citysearchname = document.getElementById("citysearch").value;
-   
-    
-        if(arr.includes(citysearchname)){
-            document.getElementById("heading").textContent = citysearchname;
-            document.getElementById("citysearch").value = "";
-        }
-        else{
-            document.getElementById("heading").textContent = "No city found with the given input....";
-        }
 
+function clearSearch() {
+  document.getElementById("heading").style.display = "none";
+  orederedList.style.display = "block";
+}
+
+function submitCity() {
+  let cityArray = document.getElementById("cityname").value;
+  if (cityArray === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "enter the city!",
+    });
+  } else {
+    arr.push(cityArray);
+    let liElement = document.createElement("li");
+    liElement.textContent = cityArray;
+    orederedList.appendChild(liElement);
+    document.getElementById("cityname").value = "";
+  }
+}
+
+function submitSearch() {
+  let citySearchName = document.getElementById("citysearch").value;
+  if (citySearchName === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "enter the city!",
+    });
+  } else {
+    if (arr.includes(citySearchName)) {
+      orederedList.style.display = "none";
+      document.getElementById("heading").textContent = citySearchName;
+      clearInputValue();
+    } else {
+      orederedList.style.display = "none";
+      document.getElementById("heading").textContent =
+        "No city found with the given input....";
+      clearInputValue();
+    }
+  }
+}
+
+function clearList() {
+  document.querySelector("ul").innerHTML = "";
 }

9ff05e0eac7164a006eddda90d9ca5440dfcb247
himamphb <himamphb124@gmail.com>
2023-05-14 23:36:18 +0530

    code

--- Projects/DOM/contact/contact.js
+++ Projects/DOM/contact/contact.js
@@ -1,10 +1,47 @@
-let addressBook = {
-    name : [],
-    number : []
+
+function addContact(fullName) {
+  if (!fullName || !fullName.trim()) return;
+
+  const contactDiv = document.createElement("div");
+  contactDiv.classList.add("contact-entry");
+
+  contactDiv.addEventListener('dblclick', function () {
+    contactDiv.remove();
+  });
+ 
+
+  const nameInitialDiv = document.createElement("div");
+  nameInitialDiv.classList.add("name-initial");
+
+  nameInitialDiv.textContent = fullName.charAt(0);
+   nameInitialDiv.style.backgroundColor = randomColor();
+
+  const fullNameDiv = document.createElement("div");
+  fullNameDiv.classList.add("full-name");
+  fullNameDiv.textContent = fullName;
+
+  contactDiv.append(nameInitialDiv);
+  contactDiv.append(fullNameDiv);
+  document.querySelector("#contacts-list").append(contactDiv);
 }
 
-function inputData(){
-    let inputNumber = document.getElementById("number").value;
-    let inputName = document.getElementById("name").value;
-    addressBook.number.push()
+function submitData(){
+    let nameInput = document.getElementById('name-input').value;
+    addContact(nameInput);
+    document.getElementById('name-input').value = "";
+}
+
+function randomColor() {
+    const colors = [
+        '#75026e',
+        '#059c16',
+        '#000770',
+        '#7a3e02',
+        '#036b64',
+        '#505e2d',
+        '#e30909',
+        '#b05e0c'
+    ];
+    
+    return colors [Math. floor(Math.random() * colors.length)];
 }

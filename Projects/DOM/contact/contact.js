85d0f623e453de46d86700af004bf88935d45e44
himamphb <himamphb124@gmail.com>
2023-05-14 17:46:49 +0530

    code

---
+++ Projects/DOM/contact/contact.js
@@ -0,0 +1,10 @@
+let addressBook = {
+    name : [],
+    number : []
+}
+
+function inputData(){
+    let inputNumber = document.getElementById("number").value;
+    let inputName = document.getElementById("name").value;
+    addressBook.number.push()
+}
--- Projects/DOM/cuberoot/cuberoot.js
+++ Projects/DOM/cuberoot/cuberoot.js
@@ -1,20 +1,11 @@
 let cubicRoot = {
   sqrt: function (num) {
-    if (num < 0 || isNaN(num)) {
-      return NaN;
-    }
-    let square_root = num / 2;
-    let temp = 0;
-    while (square_root != temp) {
-      temp = square_root;
-
-      square_root = (num / square_root + square_root) / 2;
-    }
-    return square_root;
+    return console.log(num ** (1/2));
   },
   cbrt: function (num){
-       return console.log(Math.pow(this.sqrt(num),3)); 
+       return console.log(num ** (1/3));
   }
 };
 
-cubicRoot.cbrt(3)
+cubicRoot.cbrt(125);
+cubicRoot.sqrt(16)

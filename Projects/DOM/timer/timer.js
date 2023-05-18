66caf5af8463fce4893a3e8de11ba852a0fcab28
himamphb <himamphb124@gmail.com>
2023-05-18 18:38:13 +0530

    commit

--- Projects/DOM/timer/timer.js
+++ Projects/DOM/timer/timer.js
@@ -2,37 +2,40 @@ let globalValue = 1;
 let initialValue = 0;
 let time;
 let secValue = initialValue;
-function setTimerSeconds(){
-   time = setInterval(function(){
+function setTimerSeconds() {
+  time = setInterval(function () {
 
     let displayDocument = document.getElementById("child3");
-    
-    if(secValue < 10){
-        secValue++;
-        displayDocument.textContent = secValue;
 
-    }else if(secValue == 10 && globalValue == 1){
-        secValue = initialValue
-        displayDocument.textContent = secValue;
-        secValue++;
-        globalValue++;
+    if (secValue < 10 && globalValue == 1) {
+      secValue++;
+      displayDocument.textContent = secValue;
 
-    }else if(secValue >= 10 && secValue < 20){
-        secValue++;
-        displayDocument.textContent = secValue;
-        
-    }else if(secValue == 20 && globalValue == 2){
-        secValue = initialValue;
-        displayDocument.textContent = secValue;
-        secValue++;
-        globalValue++;
-    }else if(secValue >= 20 && secValue < 30){
-        secValue++;
-        displayDocument.textContent = secValue;
-    }else if(secValue == 30){
-        secValue = initialValue;
-        displayDocument.textContent = secValue;
-        clearInterval(time);
+    } else if (secValue == 10 && globalValue == 1) {
+      globalValue++;
+      secValue = initialValue;
+      
+      displayDocument.textContent = secValue;
+    } 
+    else if (secValue < 20 && globalValue == 2) {
+      secValue++;
+       displayDocument.textContent = secValue;
+      
+
+    }else if (secValue == 20 && globalValue == 2) {
+      globalValue++;
+      secValue = initialValue;
+      displayDocument.textContent = initialValue;
+    } 
+    else if (secValue < 30 && globalValue == 3) {
+      secValue++;
+      displayDocument.textContent = secValue;
+
+    } else if (secValue == 30) {
+      secValue = initialValue;
+      displayDocument.textContent = "0:0";
+      clearInterval(time);
     }
-    },1000)
+
+  }, 1000);
 }
---
+++ Projects/ErrorHandling/index.js
@@ -0,0 +1,54 @@
+let userNameRes = document.getElementById("name");
+let userNum = document.getElementById("number");
+let errorName = document.getElementById("error-name-text");
+let errorNumber = document.getElementById("error-number-text");
+
+function submitInputData() {
+  let userName = document.getElementById("name").value;
+  let number = document.getElementById("number").value;
+
+  try {
+    if (userName.length == 0 && number.length == 0) {
+      throw new TypeError("name and number should not empty");
+    }
+
+    if (userName.length == 0 || userName.length > 10) {
+      userNameRes.style.borderColor = "#ff0000";
+      throw new TypeError("name should be greater 0 and less than 10");
+    }
+
+    if (number.length == 0 || number.length > 10) {
+      userNum.style.borderColor = "#ff0000";
+      throw new TypeError("number should  be greater 0 and less than 10");
+    }
+  } catch (e) {
+    if (e.message == "name and number should not empty") {
+      errorName.textContent = e.message;
+      errorName.style.color = "#ff0000";
+      errorNumber.textContent = e.message;
+      errorNumber.style.color = "#ff0000";
+      userNameRes.style.borderColor = "#ff0000";
+      userNum.style.borderColor = "#ff0000";
+    }
+    if (e.message == "name should be greater 0 and less than 10") {
+      errorName.textContent = e.message;
+      errorName.style.color = "#ff0000";
+    }
+
+    if (e.message == "number should  be greater 0 and less than 10") {
+      errorNumber.textContent = e.message;
+      errorNumber.style.color = "#ff0000";
+    }
+  }
+}
+
+function removeError(val) {
+  if (val.id == "name") {
+    errorName.textContent = "";
+    val.style.borderColor = "#000000";
+
+  } else {
+    errorNumber.textContent = "";
+    val.style.borderColor = "#000000";
+  }
+}

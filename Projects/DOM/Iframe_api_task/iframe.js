e102159fd6e703889c3bad5534a7b9884ca921f9
himamphb <himamphb124@gmail.com>
2023-06-05 18:09:08 +0530

    code

--- Projects/DOM/Iframe_api_task/iframe.js
+++ Projects/DOM/Iframe_api_task/iframe.js
@@ -1,32 +1,29 @@
-let iframe_list = document.getElementById("iframe_list");
-let getBtn = document.querySelector("input[type=button]");
+const iframe_list = document.getElementById("iframe_list");
+const getBtn = document.querySelector("input[type=button]");
 
 getBtn.addEventListener("click", function getData() {
-    let inputVal = document.getElementById("search").value;
+  const inputVal = document.getElementById("search").value;
   apiGetData(inputVal);
 });
 
 function apiGetData(val) {
-  let xhttp = new XMLHttpRequest();
+  const xhttp = new XMLHttpRequest();
   let result;
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange =  function () {
     if (this.readyState == 4 && this.status == 200) {
-     result = JSON.parse(xhttp.response);
-    //    console.log(result);
-       for(a in result){
-       if(val == a){
-         for(let i = 0; i < result[a].length; i++){
+      console.log("you have successfullu fetched the data");
+      result = JSON.parse(xhttp.response);
+      for (a in result) {
+        if (val === a) {
+          for (let i = 0; i < result[a].length; i++) {
             let iframeElement = document.createElement("iframe");
-            iframeElement.src = result[a][i];
+            iframeElement.src = result[a][i] + "?controls=0";
             iframe_list.appendChild(iframeElement);
-
-         }
-       }
-           
+          }
+        }
+      }
     }
-}
-  }
+  };
   xhttp.open("GET", "http://localhost:3000/videos", true);
   xhttp.send();
-
 }

71a7c32ed4fc8fa7abd08ab880f263f902f3d7eb
himamphb <himamphb124@gmail.com>
2023-06-06 18:26:51 +0530

    code

---
+++ Projects/DOM/Carosel/index.js
@@ -0,0 +1,10 @@
+let btnEle_1 = document.getElementById('btn1');
+let btnEle_2 = document.getElementById('btn2');
+
+btnEle_1.addEventListener('click',() => {
+    
+});
+
+btnEle_2.addEventListener('click',() => {
+    
+});

a0207a774b94b6cbc5aba98cf1384fd7a7aa7b8f
himamphb <himamphb124@gmail.com>
2023-06-15 18:49:39 +0530

    code

--- Projects/Recipe-task/index.js
+++ Projects/Recipe-task/index.js
@@ -1,7 +1,24 @@
-async function fetchData() {
-    let url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=tandoori';
-    let data = await fetch(url);
-    let jsonData = await data.json();
-    console.log(jsonData.meals[0]);
-}
+const submitBtn = document.querySelector("button");
+const divContainer = document.querySelector(".card");
+let jsonData , divClonedContainer;
+const mainDiv = document.querySelector(".main-container");
+const recipeBtn = document.querySelector('.recipeButton');
+const popupBox = document.querySelector('.popup-box');
 
+submitBtn.addEventListener("click", async () => {
+  mainDiv.innerHTML = "";
+  const inputValue = document.getElementById("search").value;
+  let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
+  let data = await fetch(url);
+   jsonData = await data.json();
+  console.log(jsonData);
+
+  jsonData.meals.forEach((element) => {
+    divClonedContainer = divContainer.cloneNode(true);
+    divClonedContainer.classList.remove("d-none");
+    divClonedContainer.querySelector("img").src = element.strMealThumb;
+    
+    divClonedContainer.querySelector(".card-title").textContent = element.strMeal;
+    mainDiv.appendChild(divClonedContainer);   
+  });
+});

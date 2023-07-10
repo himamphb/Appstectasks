be2b66ea20d1cd95e88a55e1d4873f77cdcaa52c
himamphb <himamphb124@gmail.com>
2023-07-10 22:20:13 +0530

    code

--- practice-project/src/components/ImageApi/ImageApi.jsx
+++ practice-project/src/components/ImageApi/ImageApi.jsx
@@ -1,14 +1,50 @@
 import React, { Component } from 'react'
+import Image from '../Image/Image';
 
 export default class ImageApi extends Component {
-
+constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      imageArray : []
+    };
+  }
+    getData = async () => {
+    const apiKey = "21c52fdd78d8bda4bcb6af63df51d6a6";
+    const query = "dog";
+    try {
+      
+      let response = await fetch(
+        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=10&format=json&nojsoncallback=1`
+      );
+      let responseData = await response.json();
+      let url = responseData.photos.photo.map((image) => {
+        const url = {
+          id: image.id,
+          url: `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
+        };
+        return url;
+      });
+      this.setState({ images: url });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+    componentDidMount() {
+    this.getData();
+  }
   render() {
+     let imageData = this.state.images.map((photo) => {
+      return <Image key={photo.id} link={photo.url} />;
+    });
     return (
       <div className='main-container'>
         <div className='child-container'>
         <input type="text"  onChange={(e) => this.handleChange(e.target.value)}/>
-        <div>hi</div>
+        <button>generate image</button>
         </div>
+        <div className='image'>{imageData}</div>
+        
       </div>
    
     )
